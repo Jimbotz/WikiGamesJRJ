977dd3b53d9b130044a300f07343db6272296959
Jimbotz <jaymesaidbarbosahernandez@gmail.com>
2024-06-03 21:36:12 -0600

    Creacion de la carpeta imgs y correcciones de repeticiones de codigo

---
+++ search.js
@@ -0,0 +1,60 @@
+const apiKey = '28930bc8bbca4bb49c075a2cbfdd5e7c';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const searchButton = document.getElementById('searchButton');
+  searchButton.addEventListener('click', searchGames);
+});
+
+async function searchGames() {
+  const query = document.getElementById('searchInput').value;
+  if (!query) return;
+
+  const apiUrl = `https://api.rawg.io/api/games?key=${apiKey}&search=${query}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    displaySearchResults(data.results);
+  } catch (error) {
+    console.error('Error fetching the games:', error);
+  }
+}
+
+function displaySearchResults(games) {
+  const resultsContainer = document.getElementById('searchResults');
+  resultsContainer.innerHTML = '';
+
+  games.forEach(game => {
+    const gameCard = document.createElement('div');
+    gameCard.classList.add('game-card');
+    
+    const gameImage = document.createElement('img');
+    gameImage.src = game.background_image;
+    gameCard.appendChild(gameImage);
+    
+    const gameInfo = document.createElement('div');
+    gameInfo.classList.add('info');
+    
+    const gameTitle = document.createElement('h2');
+    gameTitle.textContent = game.name;
+    gameInfo.appendChild(gameTitle);
+    
+    const gameRating = document.createElement('div');
+    gameRating.classList.add('stars');
+    gameRating.innerHTML = getStarRating(game.rating);
+    gameInfo.appendChild(gameRating);
+    
+    gameCard.appendChild(gameInfo);
+    resultsContainer.appendChild(gameCard);
+  });
+}
+
+function getStarRating(rating) {
+  const fullStars = Math.floor(rating);
+  const halfStar = rating % 1 >= 0.5 ? 1 : 0;
+  const emptyStars = 5 - fullStars - halfStar;
+
+  return '<i class="fas fa-star"></i>'.repeat(fullStars) + 
+         (halfStar ? '<i class="fas fa-star-half-alt"></i>' : '') + 
+         '<i class="far fa-star"></i>'.repeat(emptyStars);
+}
