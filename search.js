6e3d462bb8bfccb5e47cf7a7be30958eb1619663
Jimbotz <jaymesaidbarbosahernandez@gmail.com>
2024-06-03 22:10:35 -0600

    Buscador funcional con models

--- search.js
+++ search.js
@@ -46,6 +46,8 @@ function displaySearchResults(games) {
     
     gameCard.appendChild(gameInfo);
     resultsContainer.appendChild(gameCard);
+    
+    gameCard.addEventListener('click', () => showGameModal(game.id));
   });
 }
 
@@ -57,4 +59,69 @@ function getStarRating(rating) {
   return '<i class="fas fa-star"></i>'.repeat(fullStars) + 
          (halfStar ? '<i class="fas fa-star-half-alt"></i>' : '') + 
          '<i class="far fa-star"></i>'.repeat(emptyStars);
+}
+
+async function showGameModal(gameId) {
+  const modal = document.getElementById('gameModal');
+
+  try {
+    const [gameResponse, screenshotsResponse] = await Promise.all([
+      fetch(`https://api.rawg.io/api/games/${gameId}?key=${apiKey}`),
+      fetch(`https://api.rawg.io/api/games/${gameId}/screenshots?key=${apiKey}`)
+    ]);
+
+    const game = await gameResponse.json();
+    const screenshots = await screenshotsResponse.json();
+    
+    document.getElementById('modalTitle').textContent = game.name;
+    document.getElementById('modalDescription').textContent = game.description_raw || "No description available.";
+
+    // Obtener y mostrar las plataformas
+    const modalPlatforms = document.getElementById('modalPlatforms');
+    modalPlatforms.innerHTML = "<h3>Plataformas:</h3>";
+    const platformsList = document.createElement('ul');
+    game.platforms.forEach(platformInfo => {
+      const platformItem = document.createElement('li');
+      platformItem.textContent = platformInfo.platform.name;
+      platformsList.appendChild(platformItem);
+    });
+    modalPlatforms.appendChild(platformsList);
+
+    // Imágenes del carrusel
+    const carouselImages = document.getElementById('carousel-images');
+    carouselImages.innerHTML = ''; // Limpiar carrusel anterior
+    const libg = document.createElement('li');
+    libg.classList.add('splide__slide');
+    const img = document.createElement('img');
+    img.src = game.background_image;
+    libg.appendChild(img);
+    carouselImages.appendChild(libg);
+
+    screenshots.results.forEach(screenshot => {
+      const li = document.createElement('li');
+      li.classList.add('splide__slide');
+      const img = document.createElement('img');
+      img.src = screenshot.image;
+      li.appendChild(img);
+      carouselImages.appendChild(li);
+    });
+
+    modal.style.display = "block";
+
+    // Inicializar el carrusel después de agregar las imágenes
+    new Splide('#image-carousel').mount();
+  } catch (error) {
+    console.error('Error fetching game details:', error);
+  }
+
+  const closeButton = document.getElementsByClassName('close')[0];
+  closeButton.onclick = function() {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 }
